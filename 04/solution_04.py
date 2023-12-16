b60f863bfcbf5d7e1e0aedb704e5c063cf692f25
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-12-16 17:35:50 +0100

    refactor : day_04 improved

--- 04/solution_04.py
+++ 04/solution_04.py
@@ -3,36 +3,36 @@ from typing import List
 
 def solution(file: str):
     current_real_file_number = 0
+
     TRUE_FILE_NUMBER = 33
     MAX_OCURRENCES = 1
 
     with open(file, "r", encoding="utf-8") as file:
-        line = file.readline()
-        # 3n3E65A-nE65A
-
-        while line:
-            unique_chars_in_order: List[str] = []
+        while True:
+            line = file.readline()
+            if not line:
+                break
 
             line = line.strip().strip("\n")
 
             file_name: str = line.split("-")[0]
             checksum: str = line.split("-")[1]
 
+            current_checksum_char_index = 0
+
             for char in file_name:
-                if file_name.count(char) == MAX_OCURRENCES:
-                    unique_chars_in_order.append(char)
-
-            unique_chars_string = "".join(unique_chars_in_order)
-
-            if unique_chars_string != checksum:
-                continue
+                if (
+                    file_name.count(char) == MAX_OCURRENCES
+                    and char == checksum[current_checksum_char_index]
+                ):
+                    current_checksum_char_index += 1
+                else:
+                    continue
 
             current_real_file_number += 1
 
             if current_real_file_number == TRUE_FILE_NUMBER:
                 return checksum
-
-            line = file.readline()
 
 
 if __name__ == "__main__":
